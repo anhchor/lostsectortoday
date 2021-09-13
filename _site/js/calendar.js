d176eca201a069602db11ce2b6821fb526acb840
anh <15524956+anhchor@users.noreply.github.com>
2021-09-13 16:44:46 -0700

    clean up calendar.js, rename rotation file

--- _site/js/calendar.js
+++ _site/js/calendar.js
@@ -1,9 +1,7 @@
 const dropRotation = ["Chest", "Helmet", "Legs", "Arms"]
 const sectorRotation = [
   "Bay of Drowned Wishes", "Chamber of Starlight", "Aphelion's Rest", 
-  "The Empty Tank", 
-  // "Exodus Garden 2A", "Veles Labyrinth",
-  // "The Quarry", "Scavenger's Den", "Excavation Site XII",
+  "The Empty Tank",
   "K1 Logistics", "K1 Communion", "K1 Crew Quarters", "K1 Revelation",
   "Concealed Void", "Bunker E15", "Perdition"
 ]
@@ -16,31 +14,8 @@ const sectorRotationSep = [
 ]
 
 
-let today = new Date();
-const options = { year: 'numeric', month: 'short', day: 'numeric' };
-today = today.toLocaleDateString('en-US', options);
 
 
-// let dateWrapper = document.querySelector('.header__date');
-// dateWrapper.textContent = today;
-
-let seasonLostStart = new Date('2021-08-24T10:00:00');
-let seasonLostEnd = new Date('2022-02-22T10:00:00');
-
-let now = Date.now();
-let elapsed = now - seasonLostStart;
-let timeLeft = seasonLostEnd - now;
-
-function toDays(x) {
-  x = x / 1000 / 60 / 60 / 24;
-  x = Math.floor(x + 1);
-  return x;
-}
-
-// elapsed = toDays(elapsed);
-
-
-elapsed = 3;
 let currentDay;
 
 let dropId = 0; 
@@ -49,20 +24,6 @@ let sectorId = 0;
 let todayDrop;
 let todaySector;
 
-const getTodayId = function(x) {
-  let arrayId = 0;
-  for (i = 1; i <= elapsed; i++) {
-    if (i != arrayId) {
-      if (arrayId < x.length) {
-        arrayId += 1;
-      } else {
-        arrayId = 1;
-      }
-    }
-  }
-  arrayId -= 1; // get zero-index
-  return arrayId;
-}
 
 const getId = function(x) {
   let arrayId = 0;
@@ -80,26 +41,11 @@ const getId = function(x) {
 }
 
 
-function rotate() {
-  todayDrop = dropRotation[getTodayId(dropRotation)];
-  todaySector = sectorRotation[getTodayId(sectorRotation)];
-}
-
 function cycle() {
   todayDrop = dropRotation[getId(dropRotation)];
   todaySector = sectorRotationSep[getId(sectorRotationSep)];
 }
 
-function build() {
-  for (i = 0; i < 7; i++) {
-    rotate();
-
-    console.log(`Today is Day ${elapsed} of the season.`);
-    console.log(`Today's Legend Lost Sector is ${todaySector} and it drops ${todayDrop}.`);
-    // elapsed += 1;
-    console.log(elapsed);
-  }
-}
 
 
 function toSlug(text) {
@@ -117,7 +63,6 @@ let calendar = document.querySelector('.calendar');
 
 function buildDay() {
   let todaySectorSlug = toSlug(todaySector);
-  console.log(todaySectorSlug);
 
   let day = document.createElement('li');
   day.classList.add('day', 'day--active');
@@ -129,39 +74,42 @@ function buildDay() {
 }
 
 
-function newDayAugust() {
-  currentDay = 1;
-  for (i = 0; i < 23; i++) {
-    let day = document.createElement('li');
-    day.classList.add('day', 'day--disabled');
-    calendar.appendChild(day);
-  }
-  for (i = 0; i <= 9; i++) {
-    cycle();
+// function newDayAugust() {
+//   currentDay = 1;
+//   for (i = 0; i < 23; i++) {
+//     let day = document.createElement('li');
+//     day.classList.add('day', 'day--disabled');
+//     calendar.appendChild(day);
+//   }
+//   for (i = 0; i <= 9; i++) {
+//     cycle();
 
-    buildDay();
+//     buildDay();
 
-    currentDay += 1;
-  }
-}
+//     currentDay += 1;
+//   }
+// }
 
 
 function newDaySep() {
   currentDay = 1;
 
   for (i = 0; i < 3; i++) {
+    // greyed out days from august
     let day = document.createElement('li');
     day.classList.add('day', 'day--disabled');
     calendar.appendChild(day);
   }
 
   for (i = 0; i < 32; i++) {
+    // september
     cycle();
     buildDay();
     currentDay += 1;
   }
 
   for (i = 0; i < 2; i++) {
+    // greyed out days from october
     let day = document.createElement('li');
     day.classList.add('day', 'day--disabled');
     calendar.appendChild(day);
--- js/rotation.js
+++ js/rotation.js
@@ -1,30 +1,27 @@
-const dropRotation = ["Chest", "Helmet", "Legs", "Arms"]
-const sectorRotation = [
+const dropList = ["Chest", "Helmet", "Legs", "Arms"]
+const sectorList = [
   "Bay of Drowned Wishes", "Chamber of Starlight", "Aphelion's Rest", 
   "The Empty Tank",
   "K1 Logistics", "K1 Communion", "K1 Crew Quarters", "K1 Revelation",
   "Concealed Void", "Bunker E15", "Perdition"
 ]
 
+
+
 let today = new Date();
 const options = { year: 'numeric', month: 'short', day: 'numeric' };
 today = today.toLocaleDateString('en-US', options);
-
-// console.log(today);
 
 
 // let dateWrapper = document.querySelector('.header__date');
 // dateWrapper.textContent = today;
 
+let seasonLostStart = new Date(Date.UTC(2021, 7, 24, 17, 0, 0));
+let seasonLostEnd = new Date(Date.UTC(2022, 1, 22, 18, 0, 0));
 
-
-
-let seasonLostStart = new Date('2021-08-24T10:00:00');
-let seasonLostEnd = new Date('2022-02-22T10:00:00');
 
 let now = Date.now();
-let elapsed = now - seasonLostStart;
-let timeLeft = seasonLostEnd - now;
+let currentDayOfSeason = now - seasonLostStart;
 
 function toDays(x) {
   x = x / 1000 / 60 / 60 / 24;
@@ -32,50 +29,115 @@ function toDays(x) {
   return x;
 }
 
-// console.log('Days Since Season Start: ' + toDays(elapsed));
-// console.log('Days Left in Season: ' + toDays(timeLeft));
+currentDayOfSeason = toDays(currentDayOfSeason);
 
-elapsed = toDays(elapsed);
-
-
-let testElapsed = 4;
-let dropRotationNumber = 0; 
-let sectorRotationNumber = 0;
 
 let todayDrop;
-for (i = 1; i <= elapsed; i++) {
-  if (i != dropRotationNumber) {
-    if (dropRotationNumber < 4) {
-      dropRotationNumber += 1;
-    } else {
-      dropRotationNumber = 1;
+let todaySector;
+
+
+const getTodayId = function(x) {
+  // cycle through the list of drops/lost sectors until you get to today's day
+  // example: 
+  // today is day 11 of the season
+  // cycle to the 11th lost sector and the 11th drop
+
+  let arrayId = 0;
+  for (i = 1; i <= currentDayOfSeason; i++) {
+    if (i != arrayId) {
+      if (arrayId < x.length) {
+        arrayId += 1;
+      } else {
+        arrayId = 1;
+      }
     }
   }
+  arrayId -= 1; // get zero-index
+  return arrayId;
 }
 
-for (i = 1; i <= elapsed; i++) {
-  if (i != sectorRotationNumber) {
-    if (sectorRotationNumber < 5) {
-      sectorRotationNumber += 1;
-    } else {
-      sectorRotationNumber = 1;
-    }
+
+function toSlug(text) {
+  return text
+    .toLowerCase()
+    .replace(/ /g,'-') // replace space with dash
+    .replace(/’/g,"'") // replace curly quote with straight quote
+    ;
+}
+
+
+
+//todayDrop = dropList[getTodayId(dropList)]; // today's drop
+// todaySector = lostSectors[getTodayId(sectorList)].name; // name of today's lost sector
+//todaySectorId = getTodayId(sectorList); // id of today's lost sector
+
+
+
+todayDrop = dropList[getTodayId(dropList)]; // today's drop
+todaySectorId = getTodayId(sectorList); // id of today's lost sector
+
+let requestURL = 'js/legend.json';
+//let requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+  lostSectors = request.response;
+
+  todayDrop = dropList[getTodayId(dropList)]; // today's drop
+  todaySectorId = getTodayId(sectorList); // id of today's lost sector
+  todaySector = lostSectors[getTodayId(sectorList)].name; // name of today's lost sector
+  
+  fillInfo();
+}
+
+
+
+
+function fillInfo() {
+  // insert all this info into the html page
+  // sorry this is silly 
+
+
+  let sectorName = document.querySelector('.ls__link');
+  let readmore = document.querySelector('.readmore');
+  sectorName.textContent = todaySector;
+  todaySectorSlug = toSlug(todaySector);
+  sectorName.setAttribute("href", `sector/${todaySectorSlug}`);
+  readmore.setAttribute("href", `sector/${todaySectorSlug}`);
+
+  let sectorLocation = document.querySelector('.ls__location');
+  sectorLocation.textContent = lostSectors[todaySectorId].location;
+
+  let sectorDrop = document.querySelector('.ls__drop-item');
+  sectorDrop.textContent = todayDrop;
+
+  let sectorBurn = document.querySelector('.ls__burn');
+  sectorBurn.classList.add(`ls__element--${lostSectors[todaySectorId].burn}`);
+  sectorBurn.textContent = lostSectors[todaySectorId].burn;
+
+  let championWrapper = document.querySelector('.ls__champion-wrapper');
+
+  for (i = 0; i < lostSectors[todaySectorId].champions.length; i++) {
+    let newChampion = document.createElement('span');
+    newChampion.classList.add('ls__champion', `ls__champion--${lostSectors[todaySectorId].champions[i].name}`);
+    newChampion.textContent = lostSectors[todaySectorId].champions[i].name;
+    championWrapper.appendChild(newChampion);
   }
+
+  let shieldWrapper = document.querySelector('.ls__shield-wrapper');
+  for (i = 0; i < lostSectors[todaySectorId].shields.length; i++) {
+    let newShield = document.createElement('span');
+    newShield.classList.add('ls__shield', 'ls__element', `ls__element--${lostSectors[todaySectorId].shields[i].name}`);
+    newShield.textContent = lostSectors[todaySectorId].shields[i].name;
+    shieldWrapper.appendChild(newShield);
+  }
+
+  let modifierName = document.querySelector('.ls__modifier-name');
+  let modifierDesc = document.querySelector('.ls__modifier-desc');
+
+  modifierName.textContent = lostSectors[todaySectorId].modifiers[0].name;
+  modifierDesc.textContent = lostSectors[todaySectorId].modifiers[0].desc;
+
 }
-
-dropRotationNumberIndexed = dropRotationNumber - 1; // zero-index
-sectorRotationNumberIndexed = sectorRotationNumber - 1; // zero-index
-
-todayDrop = dropRotation[dropRotationNumberIndexed];
-todaySector = sectorRotation[sectorRotationNumberIndexed];
-
-console.log(`Today is Day ${elapsed} of the season.`);
-//console.log(`Today's sector number is ${sectorRotationNumber} and the rotation number is ${dropRotationNumber}`);
-console.log(`Today's Legend Lost Sector is ${todaySector} and it drops ${todayDrop}`);
-
-
-// let dropWrapper = document.querySelector('.ls__drop-item');
-// dropWrapper.textContent = todayDrop;
-
-
-
